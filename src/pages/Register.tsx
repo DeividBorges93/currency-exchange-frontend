9fe655781d1dbf444b0801326d02bf9400065966
DeividBorges93 <deividmborges@gmail.com>
2023-05-06 19:32:07 -0300

    feat(pages): :sparkles: starts creating the Register page. adds html of the page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+import React, { useState, useRef, FormEvent } from "react";
+import { useNavigate } from 'react-router-dom';
+import { User } from "../schemas/schemas";
+import { StatusCodes } from 'http-status-codes';
+import { validateFieldsUser } from "../utils/validateFields";
+
+
+
+export default function Register() {
+  
+
+  return (
+    <section className="global-container">
+      <div className="form-container">
+        <div className="wrap-register">
+          <form className="form-register" onSubmit={ addUser } >
+            {errors && <span className='error-message-register'>{errors.message}</span>}
+            <h1 className="form-title">Register</h1>
+            <div className="wrap-register-input">
+              <p className="title-input">Username</p>
+              <input
+                type="text"
+                id="username"
+                className="resgiter-input"
+                name="name"
+                onChange={checkValues}
+                ref={refUsername}
+              />
+            </div>
+            <div className="wrap-register-input">
+              <p className="title-input">Email</p>
+              <input
+                type="email"
+                id="email"
+                className="resgiter-input"
+                name="name"
+                onChange={checkValues}
+                ref={refEmail}
+              />
+            </div>
+            <div className="wrap-register-input">
+              <p className="title-input">Password</p>
+              <input
+                type="password"
+                id="password"
+                className="resgiter-input"
+                name="name"
+                onChange={checkValues}
+                ref={refPassword}
+              />
+            </div>
+            <div className='container-register-form-btn'>
+              <button
+                id='register-form-btn'
+                type='submit'
+              >
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="welcome-container">
+        <div className="welcome-text">
+        <p className="welcome">Welcome</p>
+        <p className="create-account-text">create account to start</p>
+        </div>
+        <div className="login-here">
+          <p className="have-account-text">Do you have an account?</p>
+          <p className="login-here-text">Login HERE</p>
+        </div>
+      </div>
+    </section>
+  )
+}
