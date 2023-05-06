631b8b963d618941e4c2b692f923284e11e04c24
DeividBorges93 <deividmborges@gmail.com>
2023-05-06 19:13:19 -0300

    feat(routes): :truck: adds routes in the App.tsx
    route /register
    
    route /login

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Register from './pages/Register';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Login from './pages/Login';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Navigate to='/login' /> } />
+          <Route path='/register' element={<Register />}/>
+          <Route path='/login' element={<Login />}/>
+
+        </Routes>
+      </Router>
     </div>
   )
 }
